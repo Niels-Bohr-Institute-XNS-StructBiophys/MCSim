bd5add57f6fb571aad4a3839f9dce3802bfa0267
martincramerpedersen <martincramerpedersen@gmail.com>
2020-06-04 10:11:31 +0200

    Add files via upload

---
+++ INSCAT.d
@@ -0,0 +1,4 @@
+points.d                 ! name of file containing points in the scatterer
+scat.d                   ! name of output file containing I(q)
+200                      ! number of q-values
+1.0                      ! q_max
